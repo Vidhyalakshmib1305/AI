9bb22ceaacb52047b3c057494ccb3bfd94f07f57
Vidhyalakshmib1305 <154789591+Vidhyalakshmib1305@users.noreply.github.com>
2024-08-16 10:45:53 +0530

    Multiclass_svm_loss_CNN

---
+++ MULTICLASS_SVM_LOSS.py
@@ -0,0 +1,10 @@
+#MULTICLASS SVM LOSS : EXAMPLE CODE
+
+
+
+def L_i_vectorized(x, y, W):
+    scores = W.dot(x)
+    margins = np.maximum(0, scores - scores[y] + 1)
+    margins[y] = 0
+    loss_i = np.sum(margins)
+    return loss_i
